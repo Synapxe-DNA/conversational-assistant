d4b4918c3f74a210f09e80b5d7ac1e455f683e21
Tsien Jin <tsien.jin@gmail.com>
2024-07-25 21:28:49 +0800

    Frontend -- Updated navbar profiles
    - Lists all existing profiles present in the browser
    - Updated profile tabs to have rightclick context to delete the profile

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,15 @@
-import { Component } from "@angular/core";
-import { NavbarLogoGroupComponent } from "./navbar-logo-group/navbar-logo-group.component";
-import { NavbarLineComponent } from "./navbar-line/navbar-line.component";
-import { NavbarProfileLinkComponent } from "./navbar-profile-links/navbar-profile-link.component";
-import { NavbarProfileCreateComponent } from "./navbar-profile-create/navbar-profile-create.component";
-import { GeneralProfile } from "../../types/profile.type";
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+} from "@angular/core"
+import { NavbarLogoGroupComponent } from "./navbar-logo-group/navbar-logo-group.component"
+import { NavbarLineComponent } from "./navbar-line/navbar-line.component"
+import { NavbarProfileLinkComponent } from "./navbar-profile-links/navbar-profile-link.component"
+import { NavbarProfileCreateComponent } from "./navbar-profile-create/navbar-profile-create.component"
+import { GeneralProfile, Profile } from "../../types/profile.type"
+import { ProfileService } from "../../services/profile/profile.service"
 
 @Component({
   selector: "app-navbar",
@@ -17,6 +23,21 @@ import { GeneralProfile } from "../../types/profile.type";
   templateUrl: "./navbar.component.html",
   styleUrl: "./navbar.component.css",
 })
-export class NavbarComponent {
-  protected readonly GeneralProfile = GeneralProfile;
+export class NavbarComponent implements OnInit {
+  profiles: Profile[] = []
+
+  constructor(
+    private profileService: ProfileService,
+    private cdr: ChangeDetectorRef,
+  ) {}
+
+  ngOnInit() {
+    this.profileService.getProfiles().subscribe((v) => {
+      this.profiles = v
+      console.log(this.profiles)
+      this.cdr.markForCheck()
+    })
+  }
+
+  protected readonly GeneralProfile = GeneralProfile
 }
