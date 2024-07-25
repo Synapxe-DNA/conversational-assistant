e8d1d7aa4b27190f07308c41cb61ae3c2056c5a2
Tsien Jin <tsien.jin@gmail.com>
2024-07-25 11:28:08 +0800

    Frontend -- created preference component

---
+++ frontend/src/app/services/preference/preference.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PreferenceService {
+  /**
+   * This service is used to set the user preference across the website.
+   */
+
+
+
+  constructor() { }
+
+
+}
