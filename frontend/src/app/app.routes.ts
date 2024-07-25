78bb5301f9965cce487750e9a44e005dc1cfdfd5
Tsien Jin <tsien.jin@gmail.com>
2024-07-25 11:27:43 +0800

    Frontend -- added routes for pages

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import {MainLayoutComponent} from "./layouts/main-layout/main-layout.component";
+import {CreateProfileComponent} from "./pages/create-profile/create-profile.component";
+import {ChatComponent} from "./pages/chat/chat.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "", component: MainLayoutComponent, children: [
+      {
+        path: 'create', component: CreateProfileComponent
+      },
+
+      {
+        path: "chat", children: [
+          {path: "", pathMatch:"full", redirectTo:"/chat/general"},
+          {path: "*", redirectTo:"/chat/general"},
+          {
+            path: ":profileId", component: ChatComponent
+          }
+        ]
+      }
+
+    ]
+  },
+  {
+    path:"**", redirectTo: "/chat/general"
+  }
+];
