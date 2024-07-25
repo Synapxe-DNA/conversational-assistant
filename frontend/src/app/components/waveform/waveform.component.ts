e1a148690a3023801dad21a428893f11bc2aaeb5
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2024-07-25 17:00:42 +0800

    Frontend -- improved wave format scaling - Used sin wave to scale midtones - Improved rendering of rightmost bar

--- frontend/src/app/components/waveform/waveform.component.ts
+++ frontend/src/app/components/waveform/waveform.component.ts
@@ -36,7 +36,9 @@ export class WaveformComponent implements AfterViewInit{
   async startAnalyser(){
     // TODO this should be done from user input
     this.stream = await this.audioService.getMicInput()
-    this.audioAnalyser = new AudioAnalyser(this.stream as MediaStream, this.bars)
+
+    // One more bar is added so that the "highest" frequency bar is attainable with regular voice
+    this.audioAnalyser = new AudioAnalyser(this.stream as MediaStream, this.bars+1)
   }
 
   mainLoop(){
@@ -45,9 +47,9 @@ export class WaveformComponent implements AfterViewInit{
       if(this.audioAnalyser){
         const barHeights = this.audioAnalyser.getFrequency()
         this.levelBars.forEach((b, index) => {
-          // Scale height of bar based on
-          b.nativeElement.style.height = `${barHeights[index] * (((index)/(index+1/2))+1) * this.container.nativeElement.clientHeight}px`
-        })
+          // Scale height of bar based on audio levels.
+          // Scaled for higher mid-tones
+          b.nativeElement.style.height = `${barHeights[index] * (1 + Math.sin((index / barHeights.length) * Math.PI)) * this.container.nativeElement.clientHeight}px`        })
       }
     } catch (e) {
       console.error(e)
