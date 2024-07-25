ad14a9f5e31e613d55ca364fe2a7fb02fa1ba6bd
Tsien Jin <tsien.jin@gmail.com>
2024-07-25 16:48:12 +0800

    Frontend -- Created voice chat component

--- frontend/src/app/components/voice/voice.component.ts
+++ frontend/src/app/components/voice/voice.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import {PreferenceService} from "../../services/preference/preference.service";
+import {WaveformComponent} from "../waveform/waveform.component";
+import {AudioService} from "../../services/audio/audio.service";
 
 @Component({
   selector: 'app-voice',
   standalone: true,
-  imports: [],
+  imports: [
+    WaveformComponent
+  ],
   templateUrl: './voice.component.html',
   styleUrl: './voice.component.css'
 })
 export class VoiceComponent {
+
+  constructor(
+    private preference:PreferenceService,
+  ) {
+  }
+
+
 
 }
