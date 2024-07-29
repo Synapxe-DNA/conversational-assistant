3ee4b7b79126926b4a8d71871add4fb8c8780112
sixiannn <sixiannn@gmail.com>
2024-07-29 11:44:59 +0800

    Reduce callbacks

--- backend/speech-to-text/test_stt.py
+++ backend/speech-to-text/test_stt.py
@@ -1,10 +1,12 @@
 from speech_to_text import speech_recognize_continuous_from_file
+import time
 
 def test_speech_recognition():
     # Path to the WAV file you want to test
-    filename = './backend/speech-to-text/samples/sg_lhl_10s_chinese.wav'
+    filename = './speech-to-text/samples/sg_lhl_12s_malay.wav'
 
     result = speech_recognize_continuous_from_file(filename)
+    end_time = time.time()
     print(result)
 
 if __name__ == "__main__":
