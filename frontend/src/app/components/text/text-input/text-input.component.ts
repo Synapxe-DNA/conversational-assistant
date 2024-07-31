3a3e3c5d86b864244d07b2af9035eadb15219a97
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2024-07-31 16:43:44 +0800

    Frontend -- Fixed input bar
    - Uses chat message service
    - Uses preference to convert chat mode to voice
    - Able to load active profile

--- frontend/src/app/components/text/text-input/text-input.component.ts
+++ frontend/src/app/components/text/text-input/text-input.component.ts
@@ -1,70 +1,62 @@
-import { Component, Input, ViewChild, ElementRef } from '@angular/core';
-import { Message, MessageSource, MessageRole } from '../../../types/message.type';
-import { LucideAngularModule } from 'lucide-angular';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import {Component, OnInit} from '@angular/core';
+import {MessageRole} from '../../../types/message.type';
+import {LucideAngularModule} from 'lucide-angular';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
+import {InputTextModule} from "primeng/inputtext";
+import {ChatMessageService} from "../../../services/chat-message/chat-message.service";
+import {createId} from "@paralleldrive/cuid2";
+import {ProfileService} from "../../../services/profile/profile.service";
+import {BehaviorSubject} from "rxjs";
+import {Profile} from "../../../types/profile.type";
+import {ActivatedRoute} from "@angular/router";
+import {Button} from "primeng/button";
+import {PreferenceService} from "../../../services/preference/preference.service";
+import {ChatMode} from "../../../types/chat-mode.type";
 
-const sources: MessageSource[] = [
-  {
-    title: "When is Your Baby Due?",
-    description: "When is Your Baby Due?",
-    url: "https://www.healthhub.sg/live-healthy/when-is-your-baby-due",
-    cover_image_url: "https://ch-api.healthhub.sg/api/public/content/8692c864101e4603868fb170c00230fe?v=534ae16e&t=livehealthyheaderimage",
-  },
-  {
-    title: "Important Nutrients: What Should You Eat More Of?",
-    description: "Important Nutrients: What Should You Eat More Of? Important Nutrients: What Should You Eat More Of? Important Nutrients: What Should You Eat More Of?",
-    url: "https://www.healthhub.sg/live-healthy/important-nutrients-what-should-you-eat-more-of",
-    cover_image_url: "https://ch-api.healthhub.sg/api/public/content/3885d0458c0a4521beb14ca352730423?v=646204cc&t=livehealthyheaderimage",
-  }
-];
 
 @Component({
   selector: 'app-text-input',
   standalone: true,
-  imports: [LucideAngularModule, FormsModule, CommonModule],
+  imports: [LucideAngularModule, FormsModule, CommonModule, InputTextModule, Button, ReactiveFormsModule],
   templateUrl: './text-input.component.html',
   styleUrl: './text-input.component.css'
 })
-export class TextInputComponent {
-  @Input() messages: Message[] = [];
-  newMessage: string = "";
-  @ViewChild("chatWindow") chatWindow!: ElementRef;
-  waitingForBotResponse: boolean = false;
+export class TextInputComponent implements OnInit{
+  profile:BehaviorSubject<Profile|undefined> = new BehaviorSubject<Profile|undefined>(undefined)
+
+  questionForm = new FormGroup({
+    "question": new FormControl("")
+  })
+
+  constructor(
+    private chatMessageService: ChatMessageService,
+    private preferences: PreferenceService,
+    private profileService: ProfileService,
+    private route: ActivatedRoute
+  ) {
+  }
+
+  ngOnInit() {
+    this.profile = this.profileService.getProfile(this.route.snapshot.paramMap.get('profileId') as string)
+    this.chatMessageService.load("general").then(console.log)
+  }
+
+  setVoiceMode(){
+    this.preferences.setChatMode(ChatMode.Voice)
+  }
 
   sendMessage() {
-    if (this.newMessage.trim()) {
-      this.messages.push({
-        id: "1",
-        profile_id: "1",
-        role: MessageRole.User,
-        message: "This is a user message.",
-        timestamp: "2023-08-01T12:00:00Z",
-        sources: [],
-      });
-      this.newMessage = "";
-      this.scrollToBottom();
-      this.waitingForBotResponse = true;
-
-      setTimeout(() => {
-        this.messages.push({
-          id: "2",
-          profile_id: "1",
-          role: MessageRole.System,
-          message: "This is a system message.",
-          timestamp: "2023-08-01T12:00:00Z",
-          sources: sources,
-        });
-        this.scrollToBottom();
-        this.waitingForBotResponse = false;
-      }, 1000);
+    if(!this.questionForm.value.question){
+      return
     }
+    this.chatMessageService.insert({
+      id: createId(),
+      profile_id: this.profile?.value?.id || "general",
+      role: MessageRole.User,
+      timestamp: `${Date.now()}`,
+      message: this.questionForm.value.question || ""
+    }).catch(console.error)
   }
 
-  scrollToBottom() {
-    try {
-      this.chatWindow.nativeElement.scrollTop =
-        this.chatWindow.nativeElement.scrollHeight;
-    } catch (err) {}
-  }
 }
