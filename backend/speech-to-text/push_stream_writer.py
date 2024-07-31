4200e1f6baad0da74436ce128a9af3bc06a5d1fe
sixiannn <sixiannn@gmail.com>
2024-07-31 17:32:59 +0800

    Added voiceChatRouter

---
+++ backend/speech-to-text/push_stream_writer.py
@@ -0,0 +1,8 @@
+def push_stream_writer(stream, filename):
+    """Simulates an audio source by pushing audio data from a WAV file into the stream."""
+    with open(filename, 'rb') as audio_file:
+        wav_header_size = 44  # Standard WAV header size
+        audio_file.read(wav_header_size)  # Skip the WAV header
+        audio_data = audio_file.read()
+        stream.write(audio_data)
+        stream.close()
