0e1bffe148f72af9f0639c2132bc74496c035ef6
sixiannn <sixiannn@gmail.com>
2024-08-01 09:25:26 +0800

    lint

--- backend/speech-to-text/push_stream_writer.py
+++ backend/speech-to-text/push_stream_writer.py
@@ -1,6 +1,6 @@
 def push_stream_writer(stream, filename):
     """Simulates an audio source by pushing audio data from a WAV file into the stream."""
-    with open(filename, 'rb') as audio_file:
+    with open(filename, "rb") as audio_file:
         wav_header_size = 44  # Standard WAV header size
         audio_file.read(wav_header_size)  # Skip the WAV header
         audio_data = audio_file.read()
